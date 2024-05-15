7275adb2085a43ab4520b0138d1aeb1b4c32a174
kbassem10 <kimobisso@gmail.com>
2024-05-15 23:54:01 +0300

    2.1

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
 typedef struct {
     char question[300];
     char option1[300];
@@ -12,61 +11,60 @@ typedef struct {
     char correct_option;
 } Questions;
 
+int size;
 
-int load_quiz(const char* file_name, Questions** question);
-int start_quiz(int size, Questions* question);
+int count_quetions(const char* file_name);
+void load_quiz(const char* file_name, Questions* question);
+void start_quiz(int size, Questions* question);
 //int report();
 
 int main(void){
-    Questions* question = NULL;
     const char* file_name = "questions.txt";
-    int questions_count = load_quiz(file_name, &question);
-
-    //printf("%i\n", questions_count);
+    int questions_count = count_quetions(file_name);
     if (questions_count == -1){
         printf("The File isn't on the correct path or name! (It should be questions.txt)\n");
     }
-
     if(questions_count > 0){
+        Questions question[size];
+        load_quiz(file_name, question);
         start_quiz(questions_count, question);
     }
 
-    free(question);
     return 0;
 }
 
-int load_quiz(const char* file_name, Questions** question){
+int count_quetions(const char* file_name){
     FILE* file = fopen(file_name, "r");
     if(!file){
         return -1;
     }
     char ch;
-    int size = 0;
 
     while ((ch = fgetc(file)) != EOF) {
         if (ch == '?') {
         size++;
         }
     }
-
-    (*question) = malloc(sizeof(Questions) * size);
-
     rewind(file);
+    return size;
+}
+
+void load_quiz(const char* file_name, Questions* question){
+    FILE* file = fopen(file_name, "r");
+
     for(int i = 0; i < size; i++){
-        fscanf(file, "%[^\n]\n", (*question)[i].question);
-        fscanf(file, "A) %[^\n]\n", (*question)[i].option1);
-        fscanf(file, "B) %[^\n]\n", (*question)[i].option2);
-        fscanf(file, "C) %[^\n]\n", (*question)[i].option3);
-        fscanf(file, "D) %[^\n]\n", (*question)[i].option4);
-        fscanf(file, " %c\n", &(*question)[i].correct_option);
+        fscanf(file, "%[^\n]\n", (question)[i].question);
+        fscanf(file, "A) %[^\n]\n", (question)[i].option1);
+        fscanf(file, "B) %[^\n]\n", (question)[i].option2);
+        fscanf(file, "C) %[^\n]\n", (question)[i].option3);
+        fscanf(file, "D) %[^\n]\n", (question)[i].option4);
+        fscanf(file, " %c\n", &(question)[i].correct_option);
 
     }
     fclose(file);
-    return size;
-
 }
 
-int start_quiz(int size, Questions* question){
+void start_quiz(int size, Questions* question){
     int score = 0;
     char answer;
 
@@ -81,13 +79,8 @@ int start_quiz(int size, Questions* question){
         }
         while(answer != 'A' && answer != 'B' && answer != 'C' && answer != 'D');
 
-        //scanf(" %c", &answer);
         if(answer ==  question[i].correct_option){
-            printf("Correct!!\n\n");
             score++;
-        }
-        else{
-            printf("Incorrect! , The correct answer is %c\n\n", question[i].correct_option);
         }
     }
     printf("\nYour total score: %d/%d\n", score, size);
